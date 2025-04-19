1a5e7b54b10ca1a2e06bf4ddd6c1ec7198b0251b
jstam2024 <jstam2023@gmail.com>
2025-04-19 18:14:13 +0300

    New component person table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = "Jstam";
+
+  person = {
+    givenName: "John",
+    surName: "Stam",
+    age: 43,
+    email: "jstam2023@aueb.gr"
+  }
 }
