b878aee00eddffda242ce6c585c22bc3edc123cd
jstam2024 <jstam2023@gmail.com>
2025-05-02 21:15:47 +0300

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { EventBindExampleComponent } from './components/event-bind-example/event
 
 @Component({
   selector: 'app-root',
-  imports: [PersonTableComponent, EventBindExampleComponent],
+  imports: [PersonTableComponent, EventBindExampleComponent, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
